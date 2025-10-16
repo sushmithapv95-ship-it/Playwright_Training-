4a49c9a3fff88ce3cf091766baf6f72313fc8a35
sushmithapv95-ship-it <sushmithapv95@gmail.com>
2025-10-16 13:06:03 +0530

    updated json file

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  // testDir: './tests',
-  testDir: './tests/UI_Special_Control/',
-
+   testDir: './tests/',
+  //testDir: './tests/UI_Special_Control',
   /* Run tests in files in parallel */
   fullyParallel: true,
   workers:1,
